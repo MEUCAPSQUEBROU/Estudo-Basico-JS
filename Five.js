543392e5d1af6209e5ffbd1ca2da2f8c7066bdee
MEUCAPSQUEBROU <juandantas27.02@gmail.com>
2025-03-24 20:54:05 -0300

    Delete Five.js

--- Five.js
+++
@@ -1,9 +0,0 @@
-const list = [1, 2, 3, 4, 5];
-const soma = list.reduce((total, num) => total + num, 0);
-const filt = list.filter(num => num <= 3);
-const somafilt = filt.reduce((total, num) => total + num, 0);
-
-console.log('A lista incialmente deficina é: ', list);
-console.log('Os tres primeiros numeros da lista são: ', filt);
-console.log('A soma dos elementos da lista é: ', soma);
-console.log('A soma dos tres primeiros numeros é: ', somafilt);
